20db982478c07dd60f506519673d887220e8c0d4
Patryk Jezierski <wacool00@gmail.com>
2020-01-31 12:28:10 +0100

    Added vuetify internationalization

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,15 @@
 import '@mdi/font/css/materialdesignicons.css';
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import pl from 'vuetify/es5/locale/pl';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  lang: {
+    locales: {pl},
+    current: 'pl',
+  },
   icons: {
     iconfont: 'mdi',
   },
